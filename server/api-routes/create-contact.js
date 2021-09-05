33e99a9bb272c3729de81d9054622ee5cd0ada3e
zeckstone <aw.afriyie@gmail.com>
2021-09-05 12:30:00 -0400

    updates post route

--- server/api-routes/create-contact.js
+++ server/api-routes/create-contact.js
@@ -4,12 +4,7 @@ const { createContact } = require('../utils/db-utils');
 const router = Router();
 
 router.get('/contacts', async (req, res) => {
-    const response = await createContact({
-        _id: v4(),
-        firstName: 'Aria',
-        lastName: 'Afriyie',
-        emails: ['test@gmail.com', 'test2@gmail.com']
-    });
+    const response = await createContact(req.body.newUser);
     
     res.status(200).json(response);
 });
