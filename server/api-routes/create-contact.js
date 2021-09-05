fbcc45f4fd62453eaa4c83c9a3d4e27041ff9ae2
zeckstone <aw.afriyie@gmail.com>
2021-09-05 12:36:41 -0400

    updates response code for create-contact

--- server/api-routes/create-contact.js
+++ server/api-routes/create-contact.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/contacts', async (req, res) => {
     const response = await createContact(req.body.newUser);
     
-    res.status(200).json(response);
+    res.status(201).json(response);
 });
 
 module.exports = router;
